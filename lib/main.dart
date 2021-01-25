abc42bbe0db2c89298b4ad382be46449c53481cf
neerddev <hello.paulvin@gmail.com>
2021-01-25 11:15:02 +0100

    first comit

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter_tutorials/ui/get_it/get_it_setup.dart';
-import 'package:flutter_tutorials/ui/route/route_generator.dart';
-import 'package:flutter_tutorials/ui/views/auth_view/start_view.dart';
-import 'package:flutter_tutorials/view_models/home_view_model.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter_tutorials/ui/views/home_view.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
-  setUpGetIt();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MultiProvider(
-      providers: [
-        ChangeNotifierProvider.value(
-          value: HomeViewModel(),
-        ),
-      ],
-      child: MaterialApp(
-        navigatorKey: GeneratedRoute.navigatorKey,
-        initialRoute: StartView.routeName,
-        onGenerateRoute: GeneratedRoute.onGenerate,
-      ),
+    return MaterialApp(
+      home: HomeView(),
     );
   }
 }
--- lib/ui/get_it/get_it_setup.dart
+++
@@ -1,7 +0,0 @@
-import 'package:get_it/get_it.dart';
-
-import 'package:flutter_tutorials/services/dynamic_link_api.dart';
-
-void setUpGetIt() {
-  GetIt.I.registerSingleton<DynamicLinksApi>(DynamicLinksApi());
-}
