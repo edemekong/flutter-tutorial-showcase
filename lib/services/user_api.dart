4d30f35d419c5771e1fe8608ff4b969b19a370d3
neerddev <hello.paulvin@gmail.com>
2021-01-25 12:19:05 +0100

    finish

--- lib/services/user_api.dart
+++ lib/services/user_api.dart
@@ -1,1 +1,13 @@
+import 'dart:convert';
 
+import 'package:flutter_tutorials/models/user.dart';
+import 'package:http/http.dart' as http;
+
+class UserApi {
+  static Future<List<User>> getAllUser() async {
+    final getUser =
+        await http.get('https://jsonplaceholder.typicode.com/users');
+    final List responseBody = jsonDecode(getUser.body);
+    return responseBody.map((e) => User.fromJson(e)).toList();
+  }
+}
--- lib/ui/views/home_view.dart
+++ lib/ui/views/home_view.dart
@@ -1,8 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_tutorials/models/user.dart';
+import 'package:flutter_tutorials/services/user_api.dart';
 
 class HomeView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Users'),
+      ),
+      body: FutureBuilder<List<User>>(
+        future: UserApi.getAllUser(),
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) {
+            return Center(child: CircularProgressIndicator());
+          }
+          final users = snapshot.data;
+
+          return ListView.builder(
+            itemCount: users.length,
+            itemBuilder: (context, index) {
+              final user = users[index];
+              return ListTile(
+                title: Text(user.name),
+                subtitle: Text(user.address.street),
+              );
+            },
+          );
+        },
+      ),
+    );
   }
 }
