fe0ccf078582d99e1f2bc4d0e3531d169638740d
Paul Jeremiah <hello.paulvin@gmail.com>
2021-03-06 06:06:52 +0100

    first-commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,117 +1,213 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
-void main() {
-  runApp(MyApp());
-}
+main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
-      ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: HomeView(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class HomeView extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _HomeViewState createState() => _HomeViewState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: CustomScrollView(
+          slivers: [
+            SliverPersistentHeader(
+              delegate: PersitentAppbar(
+                appbarHeight: 200,
+              ),
+              floating: true,
+              pinned: true,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            SliverList(
+              delegate: SliverChildBuilderDelegate((_, index) => Item()),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
+}
+
+class ListItems extends StatelessWidget {
+  final ScrollController controller;
+
+  const ListItems({Key key, this.controller}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      controller: controller,
+      itemCount: 9999,
+      itemBuilder: (_, i) {
+        return Item();
+      },
+    );
+  }
+}
+
+class Item extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    var width = MediaQuery.of(context).size.width;
+    return Container(
+      height: 100,
+      width: width - 48,
+      child: RandomColorBox(),
+    ).addPadding();
+  }
+}
+
+// you are allowed to break up this widget into smaller individual ones;
+class ExtendedItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    return Container(
+      padding: EdgeInsets.symmetric(horizontal: 8),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          const SizedBox(
+            height: 8,
+          ),
+          Container(
+              height: 50,
+              width: width - 48,
+              padding: const EdgeInsets.only(bottom: 8),
+              child: RandomColorBox()),
+        ],
+      ),
+    );
+  }
+}
+
+class TitleTextSection extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: const EdgeInsets.only(right: 8),
+          height: 50,
+          width: 50,
+          child: RandomColorBox(),
+        ),
+        Text(
+          'Title Text',
+        ).addPadding(),
+      ],
+    );
+  }
+}
+
+class RandomColorBox extends StatelessWidget {
+  final Color color;
+
+  RandomColorBox()
+      : color = Colors.primaries[Random().nextInt(Colors.primaries.length)];
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: color,
+    );
+  }
+}
+
+class ShrunkItem extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        const SizedBox(height: 8),
+        Padding(
+            padding: const EdgeInsets.only(left: 8), child: TitleTextSection()),
+        const SizedBox(height: 4),
+      ],
+    );
+  }
+}
+
+extension _padding on Widget {
+  Widget addPadding() => Padding(
+        padding: EdgeInsets.all(8),
+        child: this,
+      );
+}
+
+class PersitentAppbar extends SliverPersistentHeaderDelegate {
+  final double appbarHeight;
+  final double offset;
+  final FloatingHeaderSnapConfiguration snap;
+  final tabber;
+  PersitentAppbar({
+    @required this.appbarHeight,
+    this.offset,
+    this.snap,
+    this.tabber,
+  });
+
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    final firstBarOffset = (-shrinkOffset * 0.01) - shrinkOffset;
+    final secondBarOffset = (-shrinkOffset * 0.01) - shrinkOffset + 60;
+    return Container(
+      color: Colors.white,
+      child: Stack(
+        fit: StackFit.expand,
+        children: [
+          Positioned(
+            top: secondBarOffset < -1.00 ? -1 : secondBarOffset,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Wrap(
+                children: [
+                  ShrunkItem(),
+                  ExtendedItem(),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: firstBarOffset,
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              child: Wrap(
+                children: [
+                  ExtendedItem(),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => appbarHeight;
+
+  @override
+  double get minExtent => 60;
+
+  @override
+  FloatingHeaderSnapConfiguration get snapConfiguration => snap;
+
+  @override
+  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) => true;
 }
