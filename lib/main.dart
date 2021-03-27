5c343505be12f4e4e649a0493eabb978cfd4634c
Paul Jeremiah <hello.paulvin@gmail.com>
2021-03-27 02:11:20 +0100

    finish up implementation

--- lib/main.dart
+++ lib/main.dart
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Using Extension; Fast UI & Dart Tips',
+      title: 'Customize Expandable Widget',
       theme: ThemeData(
         primaryColor: Colors.blueAccent,
         visualDensity: VisualDensity.adaptivePlatformDensity,
--- lib/pages/home_page/home_page.dart
+++ lib/pages/home_page/home_page.dart
@@ -1,21 +1,115 @@
 import 'package:flutter/material.dart';
-import '../../ui/theme.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(
-              color: Colors.red,
-            ).paddingAll(20),
+      backgroundColor: Colors.blue[900],
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            SizedBox(height: 40),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ExpandableWidget(
+                child: Column(
+                  children: List.generate(
+                      5,
+                      (index) => ListTile(
+                            title: Text('I\'m Index $index'),
+                          )),
+                ),
+              ),
+            ),
+            SizedBox(height: 40),
+            Align(
+              alignment: Alignment.topCenter,
+              child: ExpandableWidget(
+                child: Column(
+                  children: List.generate(
+                      5,
+                      (index) => ListTile(
+                            title: Text('I\'m Index $index'),
+                          )),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ExpandableWidget extends StatefulWidget {
+  final Widget child;
+
+  const ExpandableWidget({Key key, @required this.child}) : super(key: key);
+
+  @override
+  _ExpandableWidgetState createState() => _ExpandableWidgetState();
+}
+
+class _ExpandableWidgetState extends State<ExpandableWidget> {
+  bool _isExpanded = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.85,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(4),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            spreadRadius: 5,
+            blurRadius: 10,
+            color: Colors.grey.shade700.withOpacity(.3),
           ),
-          Expanded(
+        ],
+      ),
+      child: Column(
+        children: [
+          InkWell(
+            onTap: () {
+              setState(() => _isExpanded = !_isExpanded);
+            },
             child: Container(
-              color: Colors.red,
-            ).paddingVertical(20),
+              padding: EdgeInsets.all(8),
+              width: MediaQuery.of(context).size.width * 0.85,
+              height: 45,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Expandable Widget'),
+                  Icon(_isExpanded ? Icons.expand_less : Icons.expand_more),
+                ],
+              ),
+            ),
+          ),
+          Divider(height: 0),
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 400),
+            transitionBuilder: (child, animation) => SizeTransition(
+              sizeFactor: animation,
+              child: child,
+            ),
+            child: Container(
+              key: ValueKey(_isExpanded),
+              height: _isExpanded ? null : 0,
+              alignment: Alignment.topCenter,
+              child: Wrap(
+                clipBehavior: Clip.hardEdge,
+                children: [
+                  Container(
+                      child: Builder(
+                    builder: (context) => widget.child,
+                  ))
+                ],
+              ),
+            ),
           ),
         ],
       ),
