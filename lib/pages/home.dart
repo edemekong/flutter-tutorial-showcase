c342c2cb62b8d519303edbbae656defe209752fa
Paul Edemekong <hello.paulvin@gmail.com>
2022-10-04 15:18:41 +0100

    updated

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
+  const HomePage({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
