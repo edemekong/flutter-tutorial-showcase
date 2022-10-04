228fdbb7d5699952c5547aff5052b3c4cb45547a
Paul Edemekong <hello.paulvin@gmail.com>
2022-10-04 15:41:25 +0100

    setup home project

--- android/app/src/main/kotlin/com/example/test_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.flutterfairy.fluttertutorial
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/main.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:showcasing_flutter/app.dart';
-
-void main() {
-  runApp(const MyApp());
-}
