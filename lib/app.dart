5e27a9117fa6cb6514e262490d23954a833ab03e
Paul Edemekong <hello.paulvin@gmail.com>
2022-10-04 15:15:16 +0100

    updated

--- lib/app.dart
+++ lib/app.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:showcasing_flutter/pages/home.dart';
+import 'package:flutter_tutorials/pages/home.dart';
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
